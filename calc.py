5b2341506cb47ffe101349d42a9d0c0a1730fa0a
atotala <anand-totala.totala@hpe.com>
2018-07-31 00:06:17 +0530

    sample commit

---
+++ calc.py
@@ -0,0 +1,19 @@
+def add(x,y):
+    return x + y
+
+def sub(x,y):
+    return x - y
+
+def mul(x,y):
+    return x * y
+
+def div(x,y):
+    if y == 0:
+        raise ValueError("cannot diveide by zero")
+    return x/y
+
+
+print(add(2,3))
+print(sub(2,3))
+print(div(2,3))
+print(mul(2,3))
